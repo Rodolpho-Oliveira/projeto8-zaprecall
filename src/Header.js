9788d2ab18a8a5dbfdc52eed0a33b90f4d8d3075
Lígia D'Ávila Bozzi <ligia.bozzi@gmail.com>
2022-03-28 10:01:59 -0300

    preparing review

--- src/Header.js
+++
@@ -1,8 +0,0 @@
-export default function Header(){
-    return(
-        <div className="header">
-            <img alt="logo" src="./sources/logo-pequeno.png" />
-            <p>ZapRecall</p>
-        </div>
-    )
-}
--- src/index.js
+++
@@ -1,4 +0,0 @@
-import ReactDOM from "react-dom"
-import Home from "./Home"
-
-ReactDOM.render(<Home />, document.querySelector(".root"))
